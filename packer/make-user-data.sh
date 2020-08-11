38fa0c6549273a8225a9b1a5134a43fe410777db
Chris Evich <cevich@redhat.com>
2020-08-11 12:02:49 -0400

    Copy containers/podman packer setup
    This is the latest `contrib/cirrus/packer` directory from the
    `containers/podman` repository more/less as-is at the time
    of this commit.  Only (now) irrelivent documentation was
    removed.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ packer/make-user-data.sh
@@ -0,0 +1,27 @@
+
+# This script is utilized by Makefile, it's not intended to be run by humans
+
+set -eo pipefail
+
+if [[ ! -r "cidata.ssh.pub" ]]; then
+    echo "ERROR: Expectinbg to find the file $PWD/cidata.ssh.pub existing and readable.
+"
+    exit 1
+fi
+
+cat <<EOF > user-data
+#cloud-config
+timezone: US/Eastern
+growpart:
+    mode: auto
+disable_root: false
+ssh_pwauth: True
+ssh_import_id: [root]
+ssh_authorized_keys:
+    - $(cat cidata.ssh.pub)
+users:
+   - name: root
+     primary-group: root
+     homedir: /root
+     system: true
+EOF
