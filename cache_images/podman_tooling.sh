d17dbc7384dc1a15a2d3a30796ba219ecaed89e6
Chris Evich <cevich@redhat.com>
2021-12-08 10:42:07 -0500

    Install swagger binary for podman CI
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- cache_images/podman_tooling.sh
+++ cache_images/podman_tooling.sh
@@ -19,6 +19,14 @@ bigto $SUDO make install.tools
 # shellcheck disable=SC2154
 if [[ "$OS_RELEASE_ID" == "ubuntu" ]]; then
     lilto $SUDO make install.libseccomp.sudo
+else  # Fedora
+    msg "Installing swagger binary"
+    download_url=$(\
+        curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/latest | \
+        jq -r '.assets[] | select(.name | contains("linux_amd64")) | .browser_download_url')
+    $SUDO curl --fail -s -o /usr/local/bin/swagger -L'#' "$download_url"
+    $SUDO chmod +x /usr/local/bin/swagger
+    /usr/local/bin/swagger version
 fi
 
 # Make pristine for other runtime usage/expectations also save a bit
