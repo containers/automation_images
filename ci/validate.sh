71850dfc421b3cf08526612f205f516cc15b5897
Chris Evich <cevich@redhat.com>
2023-02-02 14:18:38 -0500

    Validate: Catch attempted re-use of IMG_SFX
    IMG_SFX values need to change for every image build, even within the
    same PR.  Attempt to catch re-use of a suffix before starting the
    lengthy build process (which will eventually fail on a duplicate).
    Check the `imgts` image simply because it builds very early in
    cirrus-ci and cannot be skipped with a "no_*" PR label.
    
    Also - enforce IMG_SFX change rules when building in `[CI:TOOLING]`
    mode, since those container images push with a IMG_SFX tag.  It would be
    unfortunate if an unrelated PR clobbered in-use tooling images w/ a
    breaking-change.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- ci/validate.sh
+++ ci/validate.sh
@@ -35,23 +35,35 @@ fi
 # Variable is defined by Cirrus-CI at runtime
 # shellcheck disable=SC2154
 if [[ ! "$CIRRUS_CHANGE_TITLE" =~ CI:DOCS ]] && \
-   [[ ! "$CIRRUS_CHANGE_TITLE" =~ CI:TOOLING ]] && \
    ! git diff --name-only ${CIRRUS_BASE_SHA}..HEAD | grep -q IMG_SFX; then
 
   die "Every PR that builds images must include an updated IMG_SFX file.
 Simply run 'make IMG_SFX', commit the result, and re-push."
 else
+  IMG_SFX="$(<./IMG_SFX)"
   # IMG_SFX was modified vs PR's base-branch, confirm version moved forward
   # shellcheck disable=SC2154
   v_prev=$(git show ${CIRRUS_BASE_SHA}:IMG_SFX 2>&1 || true)
   # Verify new IMG_SFX value always version-sorts later than previous value.
   # This prevents screwups due to local timezone, bad, or unset clocks, etc.
-  new_img_ver=$(awk -F 't' '{print $1"."$2}' ./IMG_SFX | cut -dz -f1)
+  new_img_ver=$(awk -F 't' '{print $1"."$2}'<<<"$IMG_SFX" | cut -dz -f1)
   old_img_ver=$(awk -F 't' '{print $1"."$2}'<<<"$v_prev" | cut -dz -f1)
   # Version-sorting of date/time mimics the way renovate will compare values
   # see https://github.com/containers/automation/blob/main/renovate/defaults.json5
   latest_img_ver=$(echo -e "$new_img_ver\n$old_img_ver" | sort -V | tail -1)
   [[ "$latest_img_ver" == "$new_img_ver" ]] || \
-    die "Updated IMG_SFX '$(<IMG_SFX)' appears to be older than previous
+    die "Updated IMG_SFX '$IMG_SFX' appears to be older than previous
 value '$v_prev'.  Please check your local clock and try again."
+
+  # IMG_SFX values need to change for every image build, even within the
+  # same PR.  Attempt to catch re-use of a tag before starting the lengthy
+  # build process (which will fail on a duplicate).  Check the imgts image
+  # simply because it builds very early in cirrus-ci and cannot be skipped
+  # with a "no_*" label.
+  existing_tags=$(skopeo list-tags docker://quay.io/libpod/imgts | jq -r -e '.Tags[]')
+  if grep -q "$IMG_SFX" <<<"$existing_tags"; then
+    echo "It's highly likely the IMG_SFX '$IMG_SFX' is being re-used."
+    echo "Don't do this.  Run 'make IMG_SFX', commit the result, and re-push".
+    exit 1
+  fi
 fi
