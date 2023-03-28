e82314452fd84c7fccbea4aa7c5ef8217d2d82da
Chris Evich <cevich@redhat.com>
2023-03-28 14:38:40 -0400

    Add zfsutils package to debian CI VMs
    Ref: https://github.com/containers/storage/pull/1535
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- cache_images/debian_packaging.sh
+++ cache_images/debian_packaging.sh
@@ -80,6 +80,7 @@ INSTALL_PACKAGES=(\
     libsystemd-dev
     libtool
     libudev-dev
+    "linux-headers-$(uname -r)"
     lsb-release
     lsof
     make
@@ -118,8 +119,12 @@ INSTALL_PACKAGES=(\
     xz-utils
     zip
     zlib1g-dev
+    zfsutils
     zstd
 )
+
+# Necessary to add zfs support needed for c/storage CI.
+$SUDO sed -i -r 's/^(deb.*)/\1 contrib/g' /etc/apt/sources.list
 
 # Necessary to update cache of newly added repos
 lilto $SUDO apt-get -q -y update
