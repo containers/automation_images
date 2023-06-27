da513e4ff789daf901c9034998b98c9757bb302b
Chris Evich <cevich@redhat.com>
2023-06-27 11:11:17 -0400

    Update CI VM images for newer packages
    Ref:
    https://github.com/containers/podman/pull/18612#issuecomment-1608089245
    
    Also:
    
    * Switch to the distro. version of `passt` since development has
      cooled down.  It's also now available in both F37 and 38.  Note, for
      the F38 images, it will still grab it from updates-testing.
    
    * Implement a few fixes to cope with the `dnf` to `dnf5` update
      when switching from F38 to rawhide.
    
    * De-duplicate & force use of `DEBIAN_FRONTEND=noninteractive` by
      including it into the `$SUDO` variable.
    
    * Add debugging to `base_images/debian_base-setup.sh` to help verify
      `DEBIAN_FRONTEND=noninteractive` is set.
    
    * Move python packages out of Fedora rawhide due to a broken
      dependnecy:
      `nothing provides (python3.12dist(astroid) <= 2.17~~dev0 with
      python3.12dist(astroid) >= 2.15.2) needed by
      python3-pylint-2.17.2-2.fc39.noarch`
    
    * Fix the name of the debian kernel headers package to not mention the
      currently booted kernel version.  This fixes an issue where an older
      kernel is in use and doesn't match currently available headers package.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- base_images/debian_base-setup.sh
+++ base_images/debian_base-setup.sh
@@ -16,8 +16,8 @@ REPO_DIRPATH=$(realpath "$SCRIPT_DIRPATH/../")
 # shellcheck source=./lib.sh
 source "$REPO_DIRPATH/lib.sh"
 
-# Switch to Debian Unstable (SID)
-cat << EOF | sudo tee /etc/apt/sources.list
+echo "Switch sources to Debian Unstable (SID)"
+cat << EOF | $SUDO tee /etc/apt/sources.list
 deb http://deb.debian.org/debian/ unstable main
 deb-src http://deb.debian.org/debian/ unstable main
 EOF
@@ -36,15 +36,15 @@ PKGS=( \
 )
 
 echo "Updating package source lists"
-$SUDO apt-get -qq -y update
+( set -x; $SUDO apt-get -qq -y update; )
 echo "Upgrading to SID"
-$SUDO apt-get -qq -y full-upgrade
+( set -x; $SUDO apt-get -qq -y full-upgrade; )
 echo "Installing basic, necessary packages."
-$SUDO apt-get -qq -y install "${PKGS[@]}"
+( set -x; $SUDO apt-get -q -y install "${PKGS[@]}"; )
 
 # compatibility / usefullness of all automated scripting (which is bash-centric)
-$SUDO DEBCONF_DB_OVERRIDE='File{'$SCRIPT_DIRPATH/no_dash.dat'}' \
-    dpkg-reconfigure dash
+( set -x; $SUDO DEBCONF_DB_OVERRIDE='File{'$SCRIPT_DIRPATH/no_dash.dat'}' \
+    dpkg-reconfigure dash; )
 
 # Ref: https://wiki.debian.org/DebianReleases
 # CI automation needs a *sortable* OS version/release number to select/perform/apply
@@ -57,19 +57,19 @@ base_major=$(cut -d. -f 1 <<<"$base_files_version")
 base_minor=$(cut -d. -f 2 <<<"$base_files_version")
 sortable_version=$(printf "%02d.%02d" $base_major $base_minor)
 echo "WARN: This is NOT an official version number.  It's for CI-automation purposes only."
-echo "VERSION_ID=\"$sortable_version\"" | \
-    $SUDO tee -a /etc/os-release
+( set -x; echo "VERSION_ID=\"$sortable_version\"" | \
+    $SUDO tee -a /etc/os-release; )
 
 install_automation_tooling
 
 if ! ((CONTAINER)); then
     custom_cloud_init
-    $SUDO systemctl enable rngd
+    ( set -x; $SUDO systemctl enable rngd; )
 
     # Cloud-config fails to enable this for some reason or another
-    $SUDO sed -i -r \
+    ( set -x; $SUDO sed -i -r \
       -e 's/^PermitRootLogin no/PermitRootLogin prohibit-password/' \
-      /etc/ssh/sshd_config
+      /etc/ssh/sshd_config; )
 fi
 
 finalize
